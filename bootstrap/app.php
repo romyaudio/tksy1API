bb264dbba52cf3bf5434bf653127110a36babebe
romyaudio <romyaudio@hotmail.com>
2020-03-05 19:11:40 -0500

    build test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,6 +59,12 @@ $app->singleton(
 
 $app->configure('app');
 
+$app->configure('mail');
+$app->alias('mailer', Illuminate\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\Mailer::class);
+$app->alias('mailer', Illuminate\Contracts\Mail\MailQueue::class);
+
+
 /*
 |--------------------------------------------------------------------------
 | Register Middleware
@@ -95,7 +101,9 @@ $app->middleware([
 
 // $app->register(App\Providers\AppServiceProvider::class);
 $app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Illuminate\Mail\MailServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+// $app-> configure ('servicios');
 
 /*
 |--------------------------------------------------------------------------
